669666f0accd47aecccf17bd648e564d9ffb4afe
Drew Pereli <drewpereli@gmail.com>
2021-08-17 17:17:53 -0700

    Remove empty blocks in factories

--- spec/factories/attempts.rb
+++ spec/factories/attempts.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :attempt do
-  end
+  factory :attempt
 end
--- spec/factories/queries.rb
+++ spec/factories/queries.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :query do
-  end
+  factory :query
 end
