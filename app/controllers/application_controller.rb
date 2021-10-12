333d471a78a4007705dfcfeb5fe58bdd7c820fb4
Drew Pereli <drewpereli@gmail.com>
2021-10-11 20:58:17 -0700

    Force authorization and scoping by default

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  include Pundit
+  after_action :verify_authorized, except: :index
+  after_action :verify_policy_scoped, only: :index
+
   before_action :ensure_authenticated
 
   def auth_header
