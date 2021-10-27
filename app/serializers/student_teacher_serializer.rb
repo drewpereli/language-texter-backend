8086a337b31de25b62b720fae4b34d63cfb6dca7
Drew Pereli <drewpereli@gmail.com>
2021-10-26 22:22:11 -0700

    Fix serializers

--- app/serializers/student_teacher_serializer.rb
+++ app/serializers/student_teacher_serializer.rb
@@ -3,15 +3,5 @@
 class StudentTeacherSerializer < ActiveModel::Serializer
   attributes :id,
              :student_id,
-             :student_username,
-             :teacher_id,
-             :teacher_username
-
-  def student_username
-    object.student.username
-  end
-
-  def teacher_username
-    object.teacher.username
-  end
+             :teacher_id
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username
+  attributes :id, :username, :student_ids, :teacher_ids
+
+  def student_ids
+    object.students.ids
+  end
+
+  def teacher_ids
+    object.teachers.ids
+  end
 end
