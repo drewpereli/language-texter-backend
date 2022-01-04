e2b428d631817d269395ee58ba08a0b869564650
Drew Pereli <drewpereli@gmail.com>
2022-01-03 20:31:50 -0800

    Don't require authentication to access languages

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class LanguagesController < ApplicationController
+  skip_before_action :ensure_authenticated, only: %i[index]
+  skip_after_action :verify_authorized, only: %i[index]
+  
   def index
     @languages = policy_scope(Language)
 
